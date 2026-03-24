1108e116444411419f9dd61a799a8ef78e9f2846
9Daniel929 <daniela4354@mytusd.org>
2026-03-24 16:19:12 -0700

    Delete FireBase/Node.js

--- FireBase/Node.js
+++
@@ -1,6 +0,0 @@
-// Import Admin SDK
-const { getDatabase } = require('firebase-admin/database');
-
-// Get a database reference to our blog
-const db = getDatabase();
-const ref = db.ref('server/saving-data/fireblog');

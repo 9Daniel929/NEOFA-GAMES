9402f136731818fe365956254afa24f2e4d280e8
9Daniel929 <daniela4354@mytusd.org>
2026-03-24 16:17:22 -0700

    Update and rename email.js to Node.js

---
+++ FireBase/Node.js
@@ -0,0 +1,6 @@
+// Import Admin SDK
+const { getDatabase } = require('firebase-admin/database');
+
+// Get a database reference to our blog
+const db = getDatabase();
+const ref = db.ref('server/saving-data/fireblog');
--- FireBase/email.js
+++
@@ -1,2 +0,0 @@
-const:user = auth.firebase.email = {"email"} locate.email 
-var:user = { example1234@gmail.com } = const:access
